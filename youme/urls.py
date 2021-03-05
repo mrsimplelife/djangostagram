d6fb3f3c039575a3b484f8b85a3f951fb6d6b4e1
youme <mrsimplelife@naver.com>
2021-03-05 12:27:42 +0900

    Add basic layout

--- youme/urls.py
+++ youme/urls.py
@@ -3,10 +3,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import URLPattern, URLResolver, include, path
+from django.views.generic import TemplateView
 import debug_toolbar
 
 urlpatterns: list[Union[URLPattern, URLResolver]] = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='root.html'), name='root')
 ]
 urlpatterns += static(settings.STATIC_URL,
                       document_root=settings.STATIC_ROOT)
